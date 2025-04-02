5f94abc372351cf59fdc58172d49aa4941613cec
merousss <tim_5678@mail.ru>
2025-04-02 15:29:36 +0300

    update bot

--- example_bot.py
+++ example_bot.py
@@ -21,8 +21,6 @@ datepicker = DatePicker()
 @dp.message(Command(commands=['start']))    # start command
 async def start(message: Message):
     
-
-
     await message.answer(
         'Select date:',
         reply_markup=await datepicker.start_calendar()
